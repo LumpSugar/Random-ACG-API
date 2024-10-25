8f21f55b74b532fbda636eca8845d81c5d7e7f8a
histion <65994410+histion@users.noreply.github.com>
2024-10-25 22:10:50 +0800

    Update index.php

--- index.php
+++ index.php
@@ -15,25 +15,25 @@
 		</div>
 		<div class="container">
 			<div class="well">
-				<p>目前PC端共收录 100+ 张图片 API最后更新时间:2022-12-10</p>
+				<p>目前背景共收录 100+ 张图片，随机图床胡乱增加中，预计最终数量一万多， API最后更新时间:2024-10-25</p>
 				<p>基本完成pc端背景个人xp化</p>
-				<p>自判断二次元API基本调用格式：</p>
+				<p>同下：</p>
 				<pre><a href="<?php $_SERVER['HTTP_HOST'] ?>/api.php" target="_blank"><?php echo 'https://'.$_SERVER['HTTP_HOST'] ?>/api.php</a></pre>
-				<p>（横屏）PC端随机二次元API</p>
+				<p>随机背景API</p>
 				<pre><a href="<?php echo 'https://'.$_SERVER['HTTP_HOST'] ?>/pc.php" target="_blank"><?php echo 'https://'.$_SERVER['HTTP_HOST'] ?>/pc.php</a></pre>
-				<p>（竖屏）手机端随机二次元API</p>
+				<p>随机二次元API</p>
 				<pre><a href="<?php echo 'https://'.$_SERVER['HTTP_HOST'] ?>/pe.php" target="_blank"><?php echo 'https://'.$_SERVER['HTTP_HOST'] ?>/pe.php</a></pre>
 				<p>请求方式</p>
 				<pre>get/post</pre>
 				<p>返回格式</p>
 				<pre>json/images</pre>
 				<p>博客</p>
-				<pre><a href="https://blog.histion.cf" target="_blank">https://blog.histion.cf</a></pre>
+				<pre><a href="https://blog.450806.xyz" target="_blank">https://blog.450806.xyz</a></pre>
 			</div>
 		</div>
 		<div class="bottom">
 
-                    <p>China © 2021-2099 <a href="https://blog.histion.cf" target="_blank" class="one-pan-link-mark">博客</a></p>
+                    <p>China © 2021-2099 <a href="https://blog.450806.xyz" target="_blank" class="one-pan-link-mark">博客</a></p>
 		</div>
 	</body>
 </html>
